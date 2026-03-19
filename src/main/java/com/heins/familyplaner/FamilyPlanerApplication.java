e6f6300549231f6936353394cd3ebfa1d402b94b
Jacob Heins <jacob.heins@accso.de>
2026-03-19 21:43:26 +0100

    Add spring boot base application

---
+++ src/main/java/com/heins/familyplaner/FamilyPlanerApplication.java
@@ -0,0 +1,13 @@
+package com.heins.familyplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyPlanerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyPlanerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heins/familyplaner/FamilyPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heins.familyplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyPlanerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
